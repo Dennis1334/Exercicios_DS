12bdc8e02f3d632e70aad43975071cfda2376036
Dennis1334 <90472903+Dennis1334@users.noreply.github.com>
2025-05-14 22:26:35 -0300

    java
    aula de java

---
+++ CaixaDialogo/src/Exercicios/CaixaDialogo.java
@@ -0,0 +1,15 @@
+package Exercicios;
+
+import javax.swing.JOptionPane;
+
+public class CaixaDialogo {
+public static void main(String[] args) {
+
+    JOptionPane.showMessageDialog(null, "Boa Noite");
+    int x = Integer.parseInt(JOptionPane.showInputDialog("Digite"));
+    System.out.println(x);
+    
+}
+
+    
+}
